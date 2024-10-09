9849ab14135bb7f8258e7665b87f69aa66444bd8
Krisztin Benedek <krisztinbeni@gmail.com>
2024-10-09 11:48:44 +0200

    uizemagyag szamitas kész? / ajax

--- 04 uzemanyagArMegtakaritas/uzemanyag.js
+++ 04 uzemanyagArMegtakaritas/uzemanyag.js
@@ -1,3 +1,5 @@
+//let rendszamPattern = /^[a-zA-Z]{3}+[100-999]{3}$/
+
 $(function(){
     let benzin = [
         {
@@ -42,7 +44,8 @@ $(function(){
     ]
 
     let uzemanyag;
-    let filled = [false, true, true, true, true, true, true, true]
+    //let filled = [false, false, true, true, true, true, true, true]
+    let filled = [false, false, false, false, false, false, false, false]
 
     $(".container").submit(function(e){   //A kitöltött űrlap elemei nem kerülnek törlésre
         e.preventDefault();
@@ -63,12 +66,61 @@ $(function(){
         }
     })
 
+    $("[name=teritesInput]").change(function(){
+        filled[1] = true;
+    })
+
+    $("[name=ftLInput]").change(function(){
+        filled[2] = true;
+    })
+
+    $("[name=kobcentiSelect]").change(function(){
+        filled[3] = true;
+    })
+
+    $("[name=rendszamInput]").change(function(){
+        filled[4] = true;
+    })
+
+    $("[name=gyartoInput]").change(function(){
+        filled[5] = true;
+    })
+
+    $("[name=honnanHovaInput]").change(function(){
+        filled[6] = true;
+    })
+
+    $("[name=megtettTavInput]").change(function(){
+        filled[7] = true;
+    })
+
     $("input, select").change(function(){
         if(!filled.includes(false)){
             $("[type=submit]").attr("disabled", false);
         }
+        /*switch(name){
+            case 0: name = "uzemanyagInput"; break;
+            case 1: name = "teritesInput"; break;
+            case 2: name = "ftLInput"; break;
+            case 3: name = "kobcentiSelect"; break;
+            case 4: name = "rendszamInput"; break;
+            case 5: name = "gyartoInput"; break;
+            case 6: name = "honnanHovaInput"; break;
+            case 7: name = "megtettTavInput"; break;
+            default: name = "Üres.";
+        }*/
     })
 
+    /*$("#rendszam").change(function(){
+        let rendszam = $(this).val().trim();
+        let typeRendszam = $(this).attr("type");
+        if (!rendszamPattern.test(rendszam) && typeRendszam == "rendszam"){
+              $(".error").html("<p>A rendszám nem megfelelő!</p>")
+        }
+        else{
+            $(".error").html("");
+        }
+    })*/
 })
 
 function getUzemanyag(t){
@@ -78,5 +130,7 @@ function getUzemanyag(t){
 }
 
 function isFilled(){
-    
+
 }
+
+
